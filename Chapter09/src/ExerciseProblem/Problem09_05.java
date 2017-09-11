2008c53de92040dadd83e743ef1312d8d9a9175d
ywlee1212 <ywlee1212@gmail.com>
2017-09-11 22:20:23 +0900

    add comment

--- Chapter09/src/ExerciseProblem/Problem09_05.java
+++ Chapter09/src/ExerciseProblem/Problem09_05.java
@@ -10,7 +10,7 @@ import java.awt.Color;
 
 public class Problem09_05 extends JFrame {
 
-	Container ct;
+	Container ct;//수정합니다.
 	JLabel[] jlbl = new JLabel[16];
 
 	Color[] color = { Color.red, Color.orange, Color.yellow, Color.GREEN, Color.cyan, Color.BLUE, Color.magenta,
